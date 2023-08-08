77a422e6dce6c7efe9a7a56ef2529b3ba3ed6b9e
ChuangStephanie <stephanie231951@yahoo.com>
2023-08-08 00:20:54 -0500

    added SelectedContact component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import './App.css'
-import ContactList from './components/ContactList';
+import { useState } from "react";
+import "./App.css";
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
 
 function App() {
-    
+  const [selectedContactId, setSelectedContactId] = useState(null);
 
-    return (
+  return (
     <>
-    
-        <ContactList />    
-    
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
     </>
-    );
+  );
 }
 
-export default App
+export default App;
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { useState, useEffect } from "react";
 import ContactRow from "./ContactRow";
 
-{/* test contacts not needed anymore
+{
+  /* test contacts not needed anymore
 const dummyContacts = [
   { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
   { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
   { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-];*/}
+];*/
+}
 
-export default function ContactList() {
+export default function ContactList({ setSelectedContactId }) {
   const [contacts, setContacts] = useState([]);
   console.log(contacts);
   const { error, setError } = useState(null);
@@ -21,9 +23,8 @@ export default function ContactList() {
           "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
         );
         const result = await response.json();
-        console.log(result)
+        console.log(result);
         setContacts(result);
-
       } catch (error) {
         setError(error);
       }
@@ -49,4 +50,3 @@ export default function ContactList() {
     </table>
   );
 }
-
